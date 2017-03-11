60a6e6c5db533c32c604d5b87a235478c9ae96c9
Josh Jahans <joshjahans@RedAnts-Air.home>
2017-03-11 12:53:34 +0000

    homescene grid filling space correctly

--- js/components/scenes/HomeScene/HomeScene.js
+++ js/components/scenes/HomeScene/HomeScene.js
@@ -9,26 +9,24 @@ export default class HomeScene extends Component {
   render () {
     return (
       <Container>
-        <Content>
-          <Grid>
-            <Row>
-              <Col style={{ backgroundColor: 'red' }}>
-                <Text>1</Text>
-              </Col>
-              <Col style={{ backgroundColor: 'blue' }}>
-                <Text>2</Text>
-              </Col>
-            </Row>
-            <Row>
-              <Col style={{ backgroundColor: 'yellow' }}>
-                <Text>3</Text>
-              </Col>
-              <Col style={{ backgroundColor: 'green' }}>
-                <Text>4</Text>
-              </Col>
-            </Row>
-          </Grid>
-        </Content>
+        <Grid>
+          <Row>
+            <Col style={{ backgroundColor: 'red' }}>
+              <Text>1</Text>
+            </Col>
+            <Col style={{ backgroundColor: 'blue' }}>
+              <Text>2</Text>
+            </Col>
+          </Row>
+          <Row>
+            <Col style={{ backgroundColor: 'yellow' }}>
+              <Text>3</Text>
+            </Col>
+            <Col style={{ backgroundColor: 'green' }}>
+              <Text>4</Text>
+            </Col>
+          </Row>
+        </Grid>
       </Container>
     )
   }
