0383fff1b4220eff195210d3d65b1d801ea77072
Josh Jahans <joshjahans@RedAnts-MacBook-Air.local>
2017-03-16 10:11:05 +0000

    Add comment to loadIcons

--- js/icons/index.js
+++ js/icons/index.js
@@ -5,8 +5,13 @@ import Icon from 'react-native-vector-icons/Ionicons'
 import icons from './icons'
 
 const iconsMap = {}
+const iconSuffixPattern = /--(active|small|large|extra-large)/g
+
+/**
+ * Extract icon image source and map result to icon name
+ * @returns {Promise}
+ */
 const loadIcons = () => {
-  const iconSuffixPattern = /--(active|small|large|extra-large)/g
   return new Promise(resolve => {
     Promise.all(Object.keys(icons).map(iconName => Icon.getImageSource(
       iconName.replace(iconSuffixPattern, ''),
